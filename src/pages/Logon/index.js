9508526376babe922c5a128fb5607925d431803a
Felipe Cooper <felipecardoso323@hotmail.com>
2020-03-26 19:37:35 -0300

    Create logon skeleton

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -2,10 +2,24 @@ import React from "react";
 
 import "./style.css";
 
+import { FiLogIn } from "react-icons/fi";
+
+import heroesImg from "../../assets/heroes.png";
+import logoImg from "../../assets/logo.svg";
 export default function Logon() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be the hero" />
+        <h1>Faça seu logon</h1>
+        <input placeholder="Sua ID" />
+        <button>Entrar</button>
+        <a href="/register">
+          <FiLogIn size="16" color="E02041" />
+          Não tenho cadastro
+        </a>
+      </section>
+      <img src={heroesImg} alt="Heroes" />
+    </div>
   );
 }
