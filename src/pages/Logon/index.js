c702d62fddcc27ebc7ba46f497d39e5f95e6c7ac
Felipe Cooper <felipecardoso323@hotmail.com>
2020-03-26 19:52:24 -0300

    Finish CSS page logon

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -10,14 +10,16 @@ export default function Logon() {
   return (
     <div className="logon-container">
       <section className="form">
-        <img src={logoImg} alt="Be the hero" />
-        <h1>Faça seu logon</h1>
-        <input placeholder="Sua ID" />
-        <button>Entrar</button>
-        <a href="/register">
-          <FiLogIn size="16" color="E02041" />
-          Não tenho cadastro
-        </a>
+        <form>
+          <img src={logoImg} alt="Be the hero" />
+          <h1>Faça seu logon</h1>
+          <input placeholder="Sua ID" />
+          <button className="button">Entrar</button>
+          <a href="/register">
+            <FiLogIn size="16" color="E02041" />
+            Não tenho cadastro
+          </a>
+        </form>
       </section>
       <img src={heroesImg} alt="Heroes" />
     </div>
