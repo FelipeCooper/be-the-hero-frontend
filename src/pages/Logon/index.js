174b020b761d0dd0f05c06d57c12a5707fb68324
Felipe Cooper <felipecardoso323@hotmail.com>
2020-03-26 20:28:24 -0300

    Create Register Page

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import "./style.css";
 
 import { FiLogIn } from "react-icons/fi";
@@ -15,10 +15,10 @@ export default function Logon() {
           <h1>Faça seu logon</h1>
           <input placeholder="Sua ID" />
           <button className="button">Entrar</button>
-          <a href="/register">
+          <Link className="back-link" to="/register">
             <FiLogIn size="16" color="E02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
       <img src={heroesImg} alt="Heroes" />
