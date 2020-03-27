d452d075b58230db1130a14654952deb0bf08b13
Felipe Cooper <felipecardoso323@hotmail.com>
2020-03-27 00:14:29 -0300

    Logon function

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,19 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./style.css";
+import { useFormik } from "formik";
 
 import { FiLogIn } from "react-icons/fi";
 
 import heroesImg from "../../assets/heroes.png";
 import logoImg from "../../assets/logo.svg";
+import api from "../../services/api";
+
 export default function Logon() {
+  const history = useHistory();
+  const formik = useFormik({
+    initialValues: {
+      id: ""
+    },
+    onSubmit: async value => {
+      try {
+        let response = await api.post("sessions", value);
+        localStorage.setItem("ongId", value);
+        localStorage.setItem("ongName", response.data.name);
+        history.push("/profile");
+      } catch (error) {
+        alert("ID não encontrada");
+      }
+    }
+  });
   return (
     <div className="logon-container">
       <section className="form">
-        <form>
+        <form onSubmit={formik.handleSubmit}>
           <img src={logoImg} alt="Be the hero" />
           <h1>Faça seu logon</h1>
-          <input placeholder="Sua ID" />
+          <input
+            name="id"
+            onChange={formik.handleChange}
+            placeholder="Sua ID"
+          />
           <button className="button">Entrar</button>
           <Link className="back-link" to="/register">
             <FiLogIn size="16" color="E02041" />
